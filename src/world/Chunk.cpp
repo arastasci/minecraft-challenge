ee8e55b7e68239aa8675c663fcaddb5ee145d65a
erenyenigul <tc.eren.yenigul@gmail.com>
2024-04-11 20:40:08 +0300

    fix: chunk typo

--- src/world/Chunk.cpp
+++ src/world/Chunk.cpp
@@ -1,8 +1,6 @@
 #include "Chunk.h"
 
-
-
-Vertex vertices_[] = {
+float vertices_[] = {
     0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
     0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
     -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
